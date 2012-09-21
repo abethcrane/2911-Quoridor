aa4ccaa3a00c784427419065067d7cbd42d98dec
blastedgnu <craigberson11@gmail.com>
2012-09-21 13:21:05 +1000

    worked on display

---
+++ Quoridor/src/quoridor/Cell.java
@@ -0,0 +1,17 @@
+package quoridor;
+
+public class Cell {
+    public boolean h;
+	
+	public boolean v;
+	
+	public int playerNum;
+	
+	public Cell() {
+		h = false;
+		v = false;
+		playerNum = 0;
+	}
+
+	
+}
---
+++ Quoridor/src/quoridor/Main.java
@@ -0,0 +1,9 @@
+package quoridor;
+
+public class Main {
+	public static void main(String[] args) {
+		Board b = new Board();
+		b.displayBoard();
+	}
+	
+}
